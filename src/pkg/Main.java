f648103260537db87453006d61848977e412a4bf
Hams Ansari <Hams Ansari@DESKTOP-623K3G2>
2018-11-21 16:42:07 +0500

    asaed

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+<<<<<<< HEAD
 		System.out.print("asddddff");
 		System.out.print("hello");
-
+=======
 		System.out.print("asddddffggg");
-
+>>>>>branch 'master' of https://github.com/mubi97/Fastfood.git
 	}
 
 }
