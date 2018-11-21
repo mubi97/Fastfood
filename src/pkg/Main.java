aa552a53ee04d9b5eecae9d4b8b957c2c7366d23
Mubashir Asaad <36328164+mubi97@users.noreply.github.com>
2018-11-21 17:19:15 +0500

    asd

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -4,13 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("asddddff");
-		System.out.print("hello");
-<<<<<<< HEAD
-		System.out.print("asddddffgggtthh");
-=======
-		System.out.print("asddddffgggsshh");
->>>>>>> refs/remotes/origin/master
+		String a = "asd";
 	}
 
 }
