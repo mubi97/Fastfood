4560ed5e3c331c363772afdf0a9037e97417c542
Hams Ansari <Hams Ansari@DESKTOP-623K3G2>
2018-11-21 17:37:39 +0500

    asd

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("asddddff");
-		System.out.print("hello");
+		System.out.print("hellsso");
 		System.out.print("asddddffgggsshh");
 		String ss = "dsssfffsd";
 
