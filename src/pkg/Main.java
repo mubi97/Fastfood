6042f82a09d38bb47a49d5a8be703c2586a49697
Hams Ansari <Hams Ansari@DESKTOP-623K3G2>
2018-11-21 17:35:12 +0500

    asd

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.print("asddddff");
 		System.out.print("hello");
 		System.out.print("asddddffgggsshh");
-		String ss = "dssssd";
+		String ss = "dsssfffsd";
 
 
 		String a = "ss";
