9490ffedf4629fe20d09bf46ebd572694a60f6e0
Hams Ansari <Hams Ansari@DESKTOP-623K3G2>
2018-11-21 17:01:09 +0500

    ddd

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.print("asddddff");
 		System.out.print("hello");
-=======
 		System.out.print("asddddffggg");
 	}
 
