38aa00a53dfbd4d4ed4089f1f3a8775afc226640
Bilal Javed <Bilal Javed@Bilal-PC>
2018-11-26 18:50:47 +0500

    Created variables

---
+++ src/pkg/Models/CustomerModel.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package pkg.Models;
+
+/**
+ * @author Bilal Javed
+ *
+ */
+public class CustomerModel {
+	private int id;
+	private String name;
+	private int points;
+	private String address;
+	private boolean featured;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	
+	public void setAddress(String address ) {
+		this.address = address;
+	}
+	
+	public void setFeatured(boolean featured) {
+		this.featured = featured;
+	}
+	
+}
