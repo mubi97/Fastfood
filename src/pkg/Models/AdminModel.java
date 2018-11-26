b5d827896a5dd046adfdb5bad9cb7a4ccfd74576
Bilal Javed <Bilal Javed@Bilal-PC>
2018-11-26 20:58:53 +0500

    admin model login model

--- src/pkg/Models/AdminModel.java
+++ src/pkg/Models/AdminModel.java
@@ -15,4 +15,20 @@ public class AdminModel {
 	private String phoneNo;
 	private String email;
 	
+	public boolean checkLogin(String email, String password) {
+		String email1 = "something@gmail.com";
+		String password1 = "12345678";
+		
+		if(password == password1 && email == email1) {
+			this.id = 123;
+			this.name = "Bilal";
+			this.address = "Charsadda";
+			this.phoneNo = "03329487904";
+			this.email = email1;
+			return true;
+		}
+		else
+			return false;
+	}
+	
 }
