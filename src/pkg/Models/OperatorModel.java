3563390e8561367e2ca1bc055753959b364ea7c0
Bilal Javed <Bilal Javed@Bilal-PC>
2018-11-26 21:02:49 +0500

    Added checkLogin to OperatorModel

--- src/pkg/Models/OperatorModel.java
+++ src/pkg/Models/OperatorModel.java
@@ -15,6 +15,22 @@ public class OperatorModel{
 	private String phoneNo;
 	private String email;
 	
+	public boolean checkLogin(String email, String password) {
+		String email1 = "something@gmail.com";
+		String password1 = "12345678";
+		
+		if(password == password1 && email == email1) {
+			this.id = 123;
+			this.name = "Bilal";
+			this.address = "Charsadda";
+			this.phoneNo = "03329487904";
+			this.email = email1;
+			return true;
+		}
+		else
+			return false;
+	}
+	
 	/*
 	public void getOpId(int id) {
 		this.id = id;
