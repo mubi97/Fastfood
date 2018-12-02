aba5270c60a46d67276960fe246760ec3d6998cc
Bilal Javed <Bilal Javed@Bilal-PC>
2018-12-03 00:40:14 +0500

    Added connection and selectQuery methods to UtilityModel

--- src/pkg/Models/UtilityModel.java
+++ src/pkg/Models/UtilityModel.java
@@ -1,6 +1,34 @@
 package pkg.Models;
 
+import java.sql.*;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class UtilityModel {
+	private Connection con;
+	private String url = "jdbc:mysql://localhost/fastfood";
+	private String user = "root";
+	private String pass = "";
+	private Statement state;
+	private ResultSet result;
 	
+	public void connection() {
+		try {
+			con = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			String msg = "Database Connection Failed!\nDetail: " + e.toString();
+			JOptionPane.showMessageDialog(new JFrame(), msg, "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		
+	}
+	
+	public ResultSet selectQuery(String query) {
+		connection();
+		state = con.createStatement();
+		result = state.executeQuery(query);
+		return result;
+	}
 
 }
